06736747d40e79436b4eb752be3375a8d2f90d9e
wimbarelds <wim.barelds@gmail.com>
2023-06-19 00:18:17 +0200

    Delete using rimraf

--- delete-modules.js
+++ delete-modules.js
@@ -2,7 +2,8 @@ console.log('-- delete-modules.js');
 const process = require('process');
 console.log('IS_CYCLIC', process.env.IS_CYCLIC ? 'true' : 'false');
 if (process.env.IS_CYCLIC) {
-  const fs = require('fs');
   const path = require('path');
-  fs.rmSync(path.resolve(__dirname, 'node_modules'), { recursive: true, force: true });
+  const { sync } = require('rimraf');
+  sync(path.resolve(__dirname, 'node_modules'));
+  console.log('Delete finished');
 }
