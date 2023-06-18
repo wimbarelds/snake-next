cb2b4aaf4355eb20a67764dfd03c7d389af531e3
wimbarelds <wim.barelds@gmail.com>
2023-06-19 00:06:58 +0200

    fix rm

--- delete-modules.js
+++ delete-modules.js
@@ -4,5 +4,5 @@ console.log('IS_CYCLIC', process.env.IS_CYCLIC ? 'true' : 'false');
 if (process.env.IS_CYCLIC) {
   const fs = require('fs');
   const path = require('path');
-  fs.rmdirSync(path.resolve(__dirname, 'node_modules'));
+  fs.rmSync(path.resolve(__dirname, 'node_modules'), { recursive: true, force: true });
 }
