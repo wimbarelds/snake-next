56fcc470ab49d72c802995befb62c76ca0d79621
wimbarelds <wim.barelds@gmail.com>
2023-06-19 01:11:52 +0200

    Change approach little more

--- renameNext.js
+++ renameNext.js
@@ -4,11 +4,20 @@ console.log('IS_CYCLIC', process.env.IS_CYCLIC ? 'true' : 'false');
 if (process.env.IS_CYCLIC) {
   const path = require('path');
   const fs = require('fs');
-  fs.renameSync(path.resolve(__dirname, '.next'), path.resolve(__dirname, '_next'));
-  console.log('Renamed finished');
-  fs.writeFileSync(
-    path.resolve(__dirname, '_next', 'package.json'),
-    JSON.stringify({ type: 'commonjs', scripts: { start: 'node standalone/server.js' } }),
+
+  fs.renameSync(path.resolve(__dirname, '.next', 'standalone'), path.resolve(__dirname, 'dist'));
+
+  fs.renameSync(
+    path.resolve(__dirname, '.next', 'static'),
+    path.resolve(__dirname, 'dist', '.next', 'static'),
   );
+  console.log('Move finished');
+
+  const packagePath = path.resolve(__dirname, 'dist', 'package.json');
+  const packageJson = JSON.parse(fs.readFileSync(packagePath, { encoding: 'utf-8' }) || '{}');
+  if (!packageJson.scripts) packageJson.scripts = {};
+  packageJson.scripts.start = 'node server.js';
+
+  fs.writeFileSync(packagePath, JSON.stringify(packageJson));
   console.log('Created package json');
 }
