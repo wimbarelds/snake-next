4c501b33b0842c8531f8399b1cf572e756c465ad
wimbarelds <wim.barelds@gmail.com>
2023-06-19 00:03:23 +0200

    require path

--- delete-modules.js
+++ delete-modules.js
@@ -3,5 +3,6 @@ const process = require('process');
 console.log('IS_CYCLIC', process.env.IS_CYCLIC ? 'true' : 'false');
 if (process.env.IS_CYCLIC) {
   const fs = require('fs');
-  fs.rmdirSync(resolve(__dirname, 'node_modules'));
+  const path = require('path');
+  fs.rmdirSync(path.resolve(__dirname, 'node_modules'));
 }
